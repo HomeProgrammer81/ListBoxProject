c9e3813db53daa393fb84f89f0efa25d91951097
HomeProgrammer81 <master@home-programming.com>
2023-01-09 10:01:37 +0900

    リストボックスに_nameで指定したものを表示

---
+++ ListBoxProject/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ListBoxProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static ObservableCollection<Fruits> FruitsList = new ObservableCollection<Fruits>()
+        {
+            new Fruits( "apple", "りんご" ),
+            new Fruits( "banana", "ばなな" ),
+            new Fruits( "orange", "みかん"),
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ListBox_FriutsList.ItemsSource = FruitsList;
+        }
+
+        /// <summary>
+        /// フルーツ
+        /// </summary>
+        private class Fruits
+        {
+            public string _id { get; set; }
+
+            public string _name { get; set; }
+
+            public Fruits(string _id, string _name)
+            {
+                this._id = _id;
+                this._name = _name;
+            }
+        }
+
+        /// <summary>
+        /// アイテムクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedIndex = ListBox_FriutsList.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                return;
+            }
+            string id = FruitsList[selectedIndex]._id;
+        }
+    }
+}
